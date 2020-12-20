9a03d907a34ae35f3b0bc6e77424ec7e1a5891a2
KleinCrafter <kleincrafter@letsmine.eu>
2020-12-20 17:36:54 +0100

    working version

--- files_clan/lib/system/event/listener/JCoinsCreateClanListener.class.php
+++ files_clan/lib/system/event/listener/JCoinsCreateClanListener.class.php
@@ -1,28 +1,44 @@
 <?php
 namespace clan\system\event\listener;
+
 use wcf\system\event\listener\IParameterizedEventListener;
 use wcf\system\user\jcoins\UserJCoinsStatementHandler;
+use wcf\system\WCF;
 
 /**
  * Add JCoins for Clans.
  *
- * @author		KleinCrafter
- * @copyright	2020 letsmine.eu
- * @license		LGPL-3.0 License
- * @package		de.wcflabs.clan.jcoins
+ * @author KleinCrafter
+ * @copyright 2020 letsmine.eu
+ * @license LGPL-3.0 License
+ * @package de.wcflabs.clan.jcoins
  */
-class JCoinsCreateClanListener implements IParameterizedEventListener {
-	/**
-	 * @inheritdoc
-	 */
-	public function execute($eventObj, $className, $eventName, array &$parameters) {
-	    if (!MODULE_JCOINS) return;
-	    
-	    if ($eventObj->getActionName() != 'create') return;
-	    
-	    foreach ($eventObj->getObjects() as $object) {
-	        UserJCoinsStatementHandler::getInstance()->create('de.wcflabs.jcoins.statement.clan', $object->getDecoratedObject());
-	    }
-	    
-	}
+class JCoinsCreateClanListener implements IParameterizedEventListener
+{
+
+    /**
+     *
+     * @inheritdoc
+     */
+    public function execute($eventObj, $className, $eventName, array &$parameters)
+    {
+        if (! MODULE_JCOINS)
+            return;
+
+        switch ($eventObj->getActionName()) {
+            case 'create':
+                UserJCoinsStatementHandler::getInstance()->create('de.wcflabs.jcoins.statement.clan', null, [
+                    'userID' => WCF::getUser()->userID
+                ]);
+
+                break;
+
+            case 'delete':
+                // delete finally, not trash bin
+                foreach ($eventObj->getObjects() as $object) {
+                    UserJCoinsStatementHandler::getInstance()->revoke('de.wcflabs.jcoins.statement.clan', $object->getDecoratedObject());
+                }
+                break;
+        }
+    }
 }
